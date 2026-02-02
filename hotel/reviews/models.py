b37e6cb50bbeef381c69de43c934ab5dd0686467
sanjaysaini16122000-ui <sanjaysaini16122000@gmail.com>
2026-02-02 16:29:18 +0530

    reviews system add

--- hotel/reviews/models.py
+++ hotel/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rooms.models import Room
 
-# Create your models here.
+class Review(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.room.room_type} ({self.rating}★)"
--- hotel/reviews/views.py
+++ hotel/reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import ReviewForm
+from rooms.models import Room
 
-# Create your views here.
+@login_required
+def add_review(request, room_id):
+    room = get_object_or_404(Room, id=room_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.room = room
+            review.save()
+            return redirect('room_detail', room_id=room.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'reviews/add_review.html', {'form': form})
